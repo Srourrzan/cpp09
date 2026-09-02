c8b7f491c5cc0e8e9eab9fc4a638c37021044691
Srourrzan <razanmsrour@gmail.com>
2026-09-02 17:19:30 +0300

    Open input file stream

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -2,8 +2,10 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <map>
+# include <string>
 # include <iostream>
 # include <algorithm>
+# include <exception>
 
 class BitcoinExchange
 {
@@ -13,6 +15,17 @@ public:
   BitcoinExchange( const BitcoinExchange & );
   BitcoinExchange( const std::map<std::string, int> );
   BitcoinExchange & operator=( const BitcoinExchange & );
+  int readEntry( std::string );
+  void addEntry( std::string, int );
+
+  class FileCannotLoad: public std::exception
+  {
+    public:
+    virtual const char* what() const throw()
+    {
+      return ("Unable to load input file\n");
+    }
+  };
 
   
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,42 @@
+#include <fstream>
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
+void read_file(char *file_name)
+{
+  std::cout << "file name: " 
+  << file_name
+  << std::endl;
+
+  std::ifstream inf(file_name);
+  if (!inf)
+    throw(BitcoinExchange::FileCannotLoad());
+  return ;
+}
+
+int validate_parameters( int argc )
 {
   if(argc < 2)
   {
     std::cerr << "provide database file in csv format\n";
-    return (1);
+    return (-1);
   }
-  
+  if (argc > 2)
+  {
+    std::cerr << "Too many passed parameters\n";
+    return (-1);
+  }
+  return (0);
+}
+
+int main(int argc, char **argv)
+{
+  if (validate_parameters(argc) < 0)
+    return (1);
+  try {
+    read_file(argv[1]);
+
+  } catch (std::exception & e) {
+    std::cout << e.what() << std::endl;
+  }
   return (0);
 }
